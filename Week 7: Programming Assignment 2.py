9fbee4f89a7cdec669ac5f403b50560b62587eb2
DharanikumaranSK <118034679+KingpinDk@users.noreply.github.com>
2023-09-11 09:22:21 +0530

    Update Week 7: Programming Assignment 2.py

--- Week 7: Programming Assignment 2.py
+++ Week 7: Programming Assignment 2.py
@@ -1,10 +1,6 @@
+import numpy as np
+
 def Transpose(M):
-    n = len(M)
-    m = len(M[0])
-    for i in range(0,n):
-        for j in range(i+1,m):
-            temp = M[i][j]
-            M[i][j] = M[j][i]
-            M[j][i] = temp
-            
-    return M
+    arrays = np.array(M)
+    num_list = arrays.transpose().tolist()
+    return num_list
